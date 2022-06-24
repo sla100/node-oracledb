4e10643adde2ac5afcebc4c6468ef7f235507127
Slawomir Osoba <slawomir.osoba@gmail.com>
2022-06-24 20:56:05 +0200

    plik do testów

---
+++ test.sh
@@ -0,0 +1,8 @@
+export NODE_ORACLEDB_CONNECTIONSTRING=localhost/xepdb1
+export NODE_ORACLEDB_USER=inesadm
+export NODE_ORACLEDB_PASSWORD=inesadm
+export NODE_ORACLEDB_PASSWORD=inesadm
+# export DPI_DEBUG_LEVEL=13
+
+# npm test
+npx mocha ./test/sosoba.mjs
